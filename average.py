7455c6f3e96ad6e488c25d7012321d75f363c3f7
lrwall87 <74141386+lrwall87@users.noreply.github.com>
2021-01-12 12:25:55 -0500

    Add files via upload

--- average.py
+++ average.py
@@ -4,6 +4,5 @@ num_2 = float(input())
 num_3 = float(input())
 num_4 = float(input())
 num_5 = float(input())
-sum = num_1 + num_2 + num_3 + num_4 + num_5
 print("The average of those numbers is:\n",
-      sum / 5)
+      (num_1 + num_2 + num_3 + num_4 + num_5) / 5)
